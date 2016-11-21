4ca184133a767d0bc751d5f9d066fd1618428cc8
Alexander Dimitrov <mnmllike@gmail.com>
2016-11-21 21:37:10 +0200

    Added in public folder stylesheets, javascript and some images, added views/user/profile.hbs

--- config/routes.js
+++ config/routes.js
@@ -4,4 +4,11 @@ module.exports = (app) => {
   app.get('/', (req, res) => {
     res.render('index')
   })
+
+  app.get('/user/profile', (req, res) => {
+    res.render('user/profile')
+  })
+  app.post('/user/profile', (req, res) => {
+    //Here is POST request
+  })
 }
