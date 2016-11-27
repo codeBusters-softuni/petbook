c32d721443e7f59e6a1e1b314faeb45c27947132
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-11-27 11:57:56 +0200

    Posts are now loaded and shown on the newsfeed

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,9 +1,23 @@
+const Post = require('mongoose').model('Post')
+
 module.exports = {
   homePageGet: (req, res) => {
     if (req.user) {
       // load all the articles that the user should see
-      let allPosts = []
-      res.render('user/newsfeed', { posts: allPosts, failedPost: req.session.failedPost })
+      Post.find({ category: req.user.category }).then(categoryPosts => {
+        Post.find({ public: true }).then(publicPosts => {
+          // save the public posts that are not already in the category posts
+          publicPosts = publicPosts.filter((item) => {  // for every public post
+            return categoryPosts.findIndex((post) => {  // get his index in categoryPosts
+              return post._id.equals(item._id)          // using _id comparison
+            }) === -1                                   // if it's -1, it's not in categoryPosts, so its left publicPosts
+          })
+          // join the two arrays
+          let postsToSee = categoryPosts.concat(publicPosts)
+
+          res.render('user/newsfeed', { posts: postsToSee, failedPost: req.session.failedPost })
+        })
+      })
     } else {
       res.render('index')
     }
