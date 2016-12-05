f21cc80395b4c0bb2bbcc0efb5665eceed63c22d
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-05 09:18:44 +0200

    Modified config.js a bit and added a capitalize function to String objects

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require('express')
 let app = express()
-const config = require('./config/config')['development']
+const config = require('./config/config').initialize()['development']
 
 require('./config/database')(config)
 require('./config/express')(app, config)
