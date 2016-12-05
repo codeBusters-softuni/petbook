062c97ab62fccbb68c6f8abc404ac5800541617c
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-05 11:12:43 +0200

    Added a helper function to HBS for identifying identical users

--- config/handlebar-helpers.js
+++ config/handlebar-helpers.js
@@ -3,8 +3,16 @@ const handlebars = require('hbs')
 
 // Tells us if the user has liked a post/photo
 let hasLiked = function (likes, user, options) {
-
   if (likes.findIndex((like) => { return like.author.equals(user) }) !== -1) {
+    return options.fn(this)
+  } else {
+    return options.inverse(this)
+  }
+}
+
+// Function that says if this is the logged in user
+let isSameUser = function (userId, userId2, options) {
+  if (userId === userId2) {
     return options.fn(this)
   } else {
     return options.inverse(this)
@@ -14,5 +22,6 @@ let hasLiked = function (likes, user, options) {
 handlebars.registerHelper({
   hasPawed: hasLiked,
   hasDisliked: hasLiked,
-  hasLoved: hasLiked
+  hasLoved: hasLiked,
+  isSameUser: isSameUser
 })
