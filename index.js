01e4d5e147279d9bee9040625cd39b74d681e577
kristina1111 <kristina.k.kovacheva@gmail.com>
2016-12-14 23:42:13 +0200

    made favicon visible near the title of the page

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let express = require('express')
 let app = express()
+var favicon = require('serve-favicon');
 let environment = process.env.ENVIRONMENT || 'development'
 const config = require('./config/config')[environment]
+
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+console.log(__dirname + '/public/images/favicon.ico')
 
 require('./config/config').initialize()
 require('./config/database')(config)
@@ -9,6 +13,9 @@ require('./config/express')(app, config)
 require('./config/routes')(app)
 require('./config/passport')()
 
+
 app.listen(1337)
 // TODO: Add links to each comment and user image
 // TODO: Add pagination!
+
+
