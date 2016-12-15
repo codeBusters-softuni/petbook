dee914b82d046f7cc3d5d82bb70c810aba70fbbd
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-15 18:39:00 +0200

    :bug: Fixed a bug where we wouldn't show the user's friends' posts when listing by category

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Post = mongoose.model('Post')
 const Category = mongoose.model('Category')
+const User = mongoose.model('User')
 const categories = require('./../config/constants').categories
 
 module.exports = {
@@ -36,27 +37,51 @@ module.exports = {
         })
       })
     } else {
-      // TODO: show friends' posts
       // user is another category
-      Category.findOne({ name: category }).then(cat => {
-        if (!cat) {
-          req.session.errorMsg = `Category ${category} does not exist!`
-          res.redirect('/')
-          return
-        }
-        Post.find({ public: true, category: cat.id }).populate('author comments likes photos').then(posts => {
-          // sort by their date descending (newest first)
-          posts = Post.sortPosts(posts)
-          // get the posts in the page
-          let postPages = Post.getPostsInPage(page, posts)
-          let postsInPage = postPages.posts
-          let pages = postPages.pages  // array of possible pages [1,2,3]
-          Post.populate(postsInPage, [{ path: 'comments.author', model: 'User' }, { path: 'author.profilePic', model: 'Photo' }]).then(() => {
-            Post.populate(postsInPage, [{ path: 'comments.author.profilePic', model: 'Photo' }]).then(() => {
-              // sorts the posts and splits their likes
-              Post.initializeForView(postsInPage).then(postsInPage => {
-                req.session.returnUrl = req.originalUrl
-                res.render('user/newsfeed', { posts: postsInPage, failedPost: req.session.failedPost, categories: categories, pages: pages })
+
+      // load all the articles from the user's friends
+      User.findById(req.user._id).then(user => {
+        let friendPostsPromises = []
+        user.friends.forEach(friend => {
+          friendPostsPromises.push(new Promise((resolve, reject) => {
+            Post.find({ author: friend }).then(posts => {
+              resolve(posts)
+            }).catch(err => reject(err))
+          }))
+        })
+        Promise.all(friendPostsPromises).then(friendPosts => {
+          friendPosts = [].concat.apply([], friendPosts)  // flatten the array of arrays into one array
+
+          Category.findOne({ name: category }).then(cat => {
+            if (!cat) {
+              req.session.errorMsg = `Category ${category} does not exist!`
+              res.redirect('/')
+              return
+            }
+            Post.find({ public: true, category: cat.id }).then(posts => {
+              // save the friend posts that are not already in our postsToSee
+              friendPosts = friendPosts.filter((item) => {
+                return posts.findIndex((post) => {
+                  return post._id.equals(item._id)
+                }) === -1
+              })
+              posts = posts.concat(friendPosts)  // add the friend posts
+              // sort by their date descending (newest first)
+              posts = Post.sortPosts(posts)
+              // get the posts in the page
+              let postPages = Post.getPostsInPage(page, posts)
+              let postsInPage = postPages.posts
+              let pages = postPages.pages  // array of possible pages [1,2,3]
+              Post.populate(postsInPage, 'author comments likes photos').then((postsInPage) => {
+                Post.populate(postsInPage, [{ path: 'comments.author', model: 'User' }, { path: 'author.profilePic', model: 'Photo' }]).then(() => {
+                  Post.populate(postsInPage, [{ path: 'comments.author.profilePic', model: 'Photo' }]).then(() => {
+                    // sorts the posts and splits their likes
+                    Post.initializeForView(postsInPage).then(postsInPage => {
+                      req.session.returnUrl = req.originalUrl
+                      res.render('user/newsfeed', { posts: postsInPage, failedPost: req.session.failedPost, categories: categories, pages: pages })
+                    })
+                  })
+                })
               })
             })
           })
