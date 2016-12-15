7a642d5378b140ebba258abfca878e94ba8949ef
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-15 15:22:39 +0200

    Refactored category-controller to work properly.
    It now correctly populates comments and profile pictures

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Post = mongoose.model('Post')
 const Category = mongoose.model('Category')
 const categories = require('./../config/constants').categories
-
+// TODO: ADD RETURN URL
 module.exports = {
   // This function should show all the articles for the given category
   // if the user is not of the same category, we should only show the public posts that are in that category!
@@ -11,28 +11,39 @@ module.exports = {
 
     if (categories.indexOf(category) === -1) {
       req.session.errorMsg = `Category ${category} does not exist!`
-      res.render('index', { categories: categories })
+      res.redirect('/')
       return
     }
 
     if (req.user.category.name === category) {
       // user is the same category
-      Post.find({ category: req.user.category.id }).populate('author comments likes').then(posts => {
-        posts = Post.initializeForView(posts)  // sorts the posts and splits their likes
-        res.render('user/newsfeed', { posts: posts, failedPost: req.session.failedPost, categories: categories })
+      Post.find({ category: req.user.category.id }).populate('author comments likes photos').then(posts => {
+        Post.populate(posts, [{ path: 'comments.author', model: 'User' }, { path: 'author.profilePic', model: 'Photo' }]).then(() => {
+          Post.populate(posts, [{ path: 'comments.author.profilePic', model: 'Photo' }]).then(() => {
+            posts = Post.initializeForView(posts).then(posts => {
+              // sorts the posts and splits their likes
+              res.render('user/newsfeed', { posts: posts, failedPost: req.session.failedPost, categories: categories })
+            })
+          })
+        })
       })
     } else {
       // user is another category
       Category.findOne({ name: category }).then(cat => {
         if (!cat) {
           req.session.errorMsg = `Category ${category} does not exist!`
-          res.render('index', { categories: categories })
+          res.redirect('/')
           return
         }
-        Post.find({ public: true, category: cat.id }).populate('author comments likes').then(posts => {
-          posts = Post.initializeForView(posts)  // sorts the posts and splits their likes
-
-          res.render('user/newsfeed', { posts: posts, failedPost: req.session.failedPost, categories: categories })
+        Post.find({ public: true, category: cat.id }).populate('author comments likes photos').then(posts => {
+          Post.populate(posts, [{ path: 'comments.author', model: 'User' }, { path: 'author.profilePic', model: 'Photo' }]).then(() => {
+            Post.populate(posts, [{ path: 'comments.author.profilePic', model: 'Photo' }]).then(() => {
+              // sorts the posts and splits their likes
+              Post.initializeForView(posts).then(posts => {
+                res.render('user/newsfeed', { posts: posts, failedPost: req.session.failedPost, categories: categories })
+              })
+            })
+          })
         })
       })
     }
