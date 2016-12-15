481900da1da3ae7c28c8094a221c7decfc7a3482
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-15 18:23:52 +0200

    Added a getPostsInPage function in Post.js It gets the current page and all the posts, picks the posts in the according pages from there and returns them, along with an array of all the possible pages

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const moment = require('moment')
 const mongoose = require('mongoose')
-
+const postsPerPage = require('../config/constants').postsPerPage
 function initializeForView (posts) {
   const Photo = mongoose.model('Photo')
   // this function initializes an array of posts to be ready to be sent to a view
@@ -26,6 +26,20 @@ function sortPosts (posts) {
     return postTwo.date - postOne.date
   })
 }
+
+function getPostsInPage (page, allPosts) {
+  // get the start and end index for the appropriate posts in the array
+  let startIndex = page * postsPerPage
+  let endIndex = startIndex + postsPerPage
+  let pageCount = Math.ceil(allPosts.length / 20)
+  let pages = pageCount.getPagesArray()  // an array of the pages - ex: [1,2,3] if we have 60 elements
+  let postsInPage = allPosts.slice(startIndex, endIndex)  // slice here for optimization
+  return {
+    posts: postsInPage,
+    pages: pages
+  }
+}
+
 let postSchema = mongoose.Schema(
   {
     content: { type: String, required: true },
@@ -106,3 +120,4 @@ const Post = mongoose.model('Post', postSchema)
 module.exports = Post
 module.exports.initializeForView = initializeForView
 module.exports.sortPosts = sortPosts
+module.exports.getPostsInPage = getPostsInPage
