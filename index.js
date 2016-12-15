dfbdb57606b8786ba61a90a2653e1687ed8fcd08
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-15 20:53:35 +0200

    Removed the favicon middleware in index.js (and moved it to express.js)

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let express = require('express')
 let app = express()
-var favicon = require('serve-favicon');
 let environment = process.env.ENVIRONMENT || 'development'
 const config = require('./config/config')[environment]
-
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 require('./config/config').initialize()
 require('./config/database')(config)
@@ -15,6 +12,5 @@ require('./config/passport')()
 
 app.listen(1337)
 // TODO: Add links to each comment and user image
-// TODO: Add pagination!
 
 
