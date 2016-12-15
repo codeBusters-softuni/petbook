3b8b3532fbef7fb7579b0c3dda06a1379b0dbd8a
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-15 15:52:34 +0200

    Added a prototype function to Number, which returns a "range" of 1 to the said number, inclusive

--- config/config.js
+++ config/config.js
@@ -12,4 +12,12 @@ module.exports.initialize = () => {
   String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1)
   }
+  Number.prototype.getPagesArray = function () {
+    // get an array of all the pages we want to display in articles/list
+    let pages = []
+    for (let i = 1; i <= this; i++) {
+      pages.push(i)
+    }
+    return pages
+  }
 }
