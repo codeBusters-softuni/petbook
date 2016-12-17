8fb709381671031af243389bbb99f4874635fbac
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-17 21:40:16 +0200

    Added a test registering a user with too long of a password

--- tests/controllers/userController-test.js
+++ tests/controllers/userController-test.js
@@ -315,6 +315,26 @@ describe('registerPost function', function () {
     }, 50)
   })
 
+  it('A user with too long of a password, should redirect only', function (done) {
+    // password should be between 4 and 20 inclusive length
+    sampleValidUser.password = '1234567891011121314151617181920'
+    requestMock.body = sampleValidUser
+    userController.registerPost(requestMock, responseMock)
+
+    setTimeout(function () {
+      expect(requestMock.session.errorMsg).to.not.be.undefined
+      expect(requestMock.session.errorMsg).to.be.equal(invalidPasswordMessage)
+      expect(responseMock.redirected).to.be.true
+      expect(responseMock.redirectUrl).to.be.equal(redirectUrl)
+
+      User.findOne({}).then(user => {
+        // Assure that no user has been created
+        expect(user).to.be.null
+        done()
+      })
+    }, 50)
+  })
+
   afterEach(function (done) {
     User.remove({}).then(() => {
       done()
