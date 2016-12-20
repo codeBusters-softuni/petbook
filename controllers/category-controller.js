90775d612f2129ccff22cac5c17defa416b766d3
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-12-20 18:42:15 +0200

    Refactored category controller's showArticles function

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -14,63 +14,54 @@ module.exports = {
       res.redirect('/')
       return
     }
-    // TODO: Refactor - populate and render the pages once!
-    if (req.user.category.name === category) {
-      // user is the same category
-      Post.find({ category: req.user.category.id }).populate('author comments likes photos').then(posts => {
-        // sort by their date descending (newest first)
-        posts = Post.sortPosts(posts)
-        // get the posts in the page
-        let postPages = Post.getPostsInPage(page, posts)
-        let postsInPage = postPages.posts
-        let pages = postPages.pages  // array of possible pages [1,2,3]
+    new Promise((resolve, reject) => {  // load the posts the user will see
+      if (req.user.category.name === category) {
+        // user is the same category
+        Post.find({ category: req.user.category.id }).populate('author comments likes photos').then(posts => {
+          // sort by their date descending (newest first)
+          posts = Post.sortPosts(posts)
+          resolve(posts)
+        })
+      } else {  // user is another category
+        // load all the articles from the user's friends
+        req.user.getPostsByFriends().then(friendPosts => {
+          Category.findOne({ name: category }).then(cat => {
+            if (!cat) {
+              req.session.errorMsg = `Category ${category} does not exist!`
+              res.redirect('/')
+              return
+            }
+            // get all the public posts of the category
+            Post.find({ public: true, category: cat.id }).then(posts => {
+              // save the friend posts that are not already in the public posts
+              friendPosts = friendPosts.filter((item) => {
+                return posts.findIndex((post) => {
+                  return post._id.equals(item._id)
+                }) === -1
+              })
+              posts = posts.concat(friendPosts)  // add the friend posts
+              resolve(posts)
+            })
+          })
+        })
+      }
+    }).then(posts => {
+      // sort by their date descending (newest first)
+      posts = Post.sortPosts(posts)
+      // get the posts in the page
+      let postPages = Post.getPostsInPage(page, posts)
+      let postsInPage = postPages.posts
+      let pages = postPages.pages  // array of possible pages [1,2,3]
+      Post.populate(postsInPage, 'author comments likes photos').then((postsInPage) => {
         Post.populate(postsInPage, [{ path: 'comments.author', model: 'User' }, { path: 'author.profilePic', model: 'Photo' }]).then(() => {
           Post.populate(postsInPage, [{ path: 'comments.author.profilePic', model: 'Photo' }]).then(() => {
+            // sorts the posts and splits their likes
             Post.initializeForView(postsInPage).then(postsInPage => {
-              // sorts the posts and splits their likes
               res.render('home/newsfeed', { posts: postsInPage, failedPost: res.locals.failedPost, categories: categories, pages: pages, selectedPage: page + 1 })
             })
           })
         })
       })
-    } else {
-      // user is another category
-
-      // load all the articles from the user's friends
-      req.user.getPostsByFriends().then(friendPosts => {
-        Category.findOne({ name: category }).then(cat => {
-          if (!cat) {
-            req.session.errorMsg = `Category ${category} does not exist!`
-            res.redirect('/')
-            return
-          }
-          Post.find({ public: true, category: cat.id }).then(posts => {
-            // save the friend posts that are not already in our postsToSee
-            friendPosts = friendPosts.filter((item) => {
-              return posts.findIndex((post) => {
-                return post._id.equals(item._id)
-              }) === -1
-            })
-            posts = posts.concat(friendPosts)  // add the friend posts
-            // sort by their date descending (newest first)
-            posts = Post.sortPosts(posts)
-            // get the posts in the page
-            let postPages = Post.getPostsInPage(page, posts)
-            let postsInPage = postPages.posts
-            let pages = postPages.pages  // array of possible pages [1,2,3]
-            Post.populate(postsInPage, 'author comments likes photos').then((postsInPage) => {
-              Post.populate(postsInPage, [{ path: 'comments.author', model: 'User' }, { path: 'author.profilePic', model: 'Photo' }]).then(() => {
-                Post.populate(postsInPage, [{ path: 'comments.author.profilePic', model: 'Photo' }]).then(() => {
-                  // sorts the posts and splits their likes
-                  Post.initializeForView(postsInPage).then(postsInPage => {
-                    res.render('home/newsfeed', { posts: postsInPage, failedPost: res.locals.failedPost, categories: categories, pages: pages, selectedPage: page + 1 })
-                  })
-                })
-              })
-            })
-          })
-        })
-      })
-    }
+    })
   }
 }
